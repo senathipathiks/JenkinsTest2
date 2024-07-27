732292089df5c8ebd14af536217ee797440ddb76
Sena <senathipathik@gmail.com>
2024-07-27 10:28:04 +0530

    first commit

---
+++ src/main/java/com/sena/MavenTest2/App.java
@@ -0,0 +1,13 @@
+package com.sena.MavenTest2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String print()
+    {
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/sena/MavenTest2/AppTest2.java
@@ -0,0 +1,11 @@
+package com.sena.MavenTest2;
+
+import junit.framework.TestCase;
+
+public class AppTest2 extends TestCase {
+
+	public void testPrint() {
+		assertEquals("Hello", App.print());
+	}
+
+}
